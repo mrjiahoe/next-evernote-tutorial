72b05e81925da00fed19bf5f21354ed7c3411f33
jiahoe <mrjiahoe@hotmail.com>
2022-02-13 21:47:20 +0800

    add notes to firebase button

--- pages/components/NoteOperations.js
+++ pages/components/NoteOperations.js
@@ -1,11 +1,17 @@
 import styles from "../../styles/Evernote.module.scss";
 import { useState } from "react";
 
+import { app, database } from "../../firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
+
 export default function NoteOperations() {
 	const [isInputVisible, setInputVisible] = useState(false);
 	const inputToggle = () => {
 		setInputVisible(!isInputVisible);
 	};
+
+	const [noteTitle, setNoteTitle] = useState("");
+	const dbInstance = collection(database, "notes");
 
 	return (
 		<>
@@ -17,11 +23,16 @@ export default function NoteOperations() {
 
 			{isInputVisible ? (
 				<div className={styles.inputContainer}>
-					<input className={styles.input} placeholder="Enter the Title..." />
+					<input
+						className={styles.input}
+						placeholder="Enter the Title..."
+						onChange={(e) => setNoteTitle(e.target.value)}
+					/>
 				</div>
 			) : (
 				<></>
 			)}
+			<button className={styles.saveBtn}>Save Note</button>
 		</>
 	);
 }
