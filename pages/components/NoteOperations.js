2d3edc3b808b12b10a9b2a5441788b49e23d02c4
jiahoe <mrjiahoe@hotmail.com>
2022-02-14 10:17:53 +0800

    and/save note w/ editor(quill)

--- pages/components/NoteOperations.js
+++ pages/components/NoteOperations.js
@@ -1,22 +1,30 @@
 import styles from "../../styles/Evernote.module.scss";
 import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 import { app, database } from "../../firebaseConfig";
 import { collection, addDoc } from "firebase/firestore";
 
 export default function NoteOperations() {
 	const [isInputVisible, setInputVisible] = useState(false);
+	const [noteTitle, setNoteTitle] = useState("");
+	const [noteDesc, setNoteDesc] = useState("");
+	const dbInstance = collection(database, "notes");
+
 	const inputToggle = () => {
 		setInputVisible(!isInputVisible);
 	};
 
-	const [noteTitle, setNoteTitle] = useState("");
-	const dbInstance = collection(database, "notes");
-
 	const saveNote = () => {
 		addDoc(dbInstance, {
 			noteTitle: noteTitle,
+			noteDesc: noteDesc,
 		});
+	};
+
+	const addDesc = (value) => {
+		setNoteDesc(value);
 	};
 
 	return (
@@ -38,6 +46,9 @@ export default function NoteOperations() {
 			) : (
 				<></>
 			)}
+			<div className={styles.ReactQuill}>
+				<ReactQuill onChange={addDesc} />
+			</div>
 			<button onClick={saveNote} className={styles.saveBtn}>
 				Save Note
 			</button>
